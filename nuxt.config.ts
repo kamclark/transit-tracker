8a5b9eefe57a8402455190372e835e6c96521068
kamari <kamnmclark@gmail.com>
2025-06-09 15:24:14 -0400

    set config for env flags on deploy for gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/transit-tracker/' : '/',
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
   css: [
